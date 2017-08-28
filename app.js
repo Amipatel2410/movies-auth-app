8b2f39438e2e631ead94c5e6529da4b10b55a3bf
amisha patel <ami.patel2410@gmail.com>
2017-08-28 10:51:46 -0400

    Initial setup of movies index

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -6,6 +7,13 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+const moviesRouter = require('./routes/movies-routes');
+app.use('/movies', moviesRouter);
 
 app.listen(PORT, () => {
   console.log(`App is up and running. Listening on port ${PORT}`);
