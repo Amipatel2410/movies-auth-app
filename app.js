b34c7450995aa567056050242b0c3a24c5a4b146
amisha patel <ami.patel2410@gmail.com>
2017-08-28 11:33:11 -0400

    Add movie model and render movie records in movies index page

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -14,6 +11,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 const moviesRouter = require('./routes/movies-routes');
 app.use('/movies', moviesRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
 
 app.listen(PORT, () => {
   console.log(`App is up and running. Listening on port ${PORT}`);
